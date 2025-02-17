17aa04abe4205b7406a8382de58a11b607692f7e
yaryan1303 <aryany1303@gmail.com>
2025-02-17 22:34:24 +0530

    first commit

---
+++ src/test/java/com/url/shortner/ShortnerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortnerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
